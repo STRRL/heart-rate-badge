6955ba07e53277f941c365a1ff55cef014cf2416
STRRL <im@strrl.dev>
2022-07-02 23:14:36 +0800

    chore: milestone
    Signed-off-by: STRRL <im@strrl.dev>

--- pages/api/latest-heart-rate.ts
+++ pages/api/latest-heart-rate.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  heartRate: Number
-}
+  heartRate: Number;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ heartRate: 233 })
+  const mysql = require("serverless-mysql")();
+  mysql.config({
+    backoff: "decorrelated",
+    base: 5,
+    cap: 200,
+    port: process.env.PORT,
+    host: process.env.ENDPOINT,
+    database: process.env.DATABASE,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+  });
+  let results = await mysql.query("SELECT * FROM mysql.db;");
+  res.status(200).json({ heartRate: 233 });
 }
